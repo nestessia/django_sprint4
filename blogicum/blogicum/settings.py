102ab2b86e1e5df2c5254ed07a5e1870e5646ca9
nestessia <nastya_kriychkova@bk.ru>
2023-05-17 16:09:10 +0300

    pepe

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -104,10 +104,10 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 
+CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
-
-CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
